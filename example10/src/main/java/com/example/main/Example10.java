f7487b7642f6fa196746427201cb1d22a95b35af
Ankita Solanki <solanki.ankita0001@gmail.com>
2025-05-19 20:14:45 +0530

    Ex:10 [Wiring Beans using method parameter]

--- example10/src/main/java/com/example/main/Example10.java
+++ example10/src/main/java/com/example/main/Example10.java
@@ -1,7 +1,18 @@
 package com.example.main;
 
+import com.example.beans.Person;
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Example10 {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        Person person = context.getBean(Person.class);
+
+        System.out.println("Person name : "+ person.getName());
+        System.out.println("Vehicle onwed by person : "+ person.getVehicle().getName());
+        System.out.println("Vehicle : "+vehicle);
     }
 }
