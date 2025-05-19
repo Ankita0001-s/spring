619dc34acad96148ea0b4fc81bf83be37ea5cd0e
Ankita Solanki <solanki.ankita0001@gmail.com>
2025-05-19 22:57:10 +0530

    Ex:11 [Wiring Beans using @Annotation on class field]

---
+++ example11/src/main/java/com/example/main/Example11.java
@@ -0,0 +1,18 @@
+package com.example.main;
+
+import com.example.beans.Person;
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example11 {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        Person person = context.getBean(Person.class);
+
+        System.out.println("Person name : "+ person.getName());
+        System.out.println("Vehicle onwed by person : "+ person.getVehicle().getName());
+        System.out.println("Vehicle : "+vehicle);
+    }
+}
